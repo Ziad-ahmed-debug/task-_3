9f41a50dbbf8cedffee8e2dbc19578f9902a490c
Ziad-ahmed-debug <zyad05467@gmail.com>
2024-07-26 15:42:03 +0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,68 @@
+const path = require("path");
+let hbs = require('hbs');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+
+const publicDirectory = path.join(__dirname, '../public');
+app.use(express.static(publicDirectory));
+
+
+const viewsDirectory = path.join(__dirname, '../temp/views');
+app.set('views', viewsDirectory);
+
+const partialsPath = path.join(__dirname, "../Temp/partials");
+hbs.registerPartials(partialsPath);
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "HOME",
+        desc: "This is home page",
+        img1: "images/1111.jpg"
+    });
+});
+
+app.get('/service', (req, res) => {
+    res.render('service', {
+        title: "SERVICE",
+        name: "Mohamed",
+        city: "Abu Hamad",
+        age: 18,
+        img2: "images/ziad.jpg"
+    });
+});
+
+app.get('/team', (req, res) => {
+    res.render('team', {
+        title: "TEAM",
+        name: "Arthur",
+        city: "cairo",
+        age: 18,
+        img3: "images/3333.png"
+    });
+});
+
+app.get("/anime", (req, res) => {
+    res.render("anime", {
+        title: "ANIME",
+        characterName: "Itachi",
+        desc: "The Uncrownded Hokagi💔",
+        city: "japan",
+        age: 20,
+        img4: "images/hhhh.jpg",
+    });
+});
+
+app.get("/car", (req, res) => {
+    res.render("car", {
+        title: "CAR",
+        desc: "The New AMG Class",
+        img5: "images/car.jpeg",
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
